0f8139f458a6d50c9a59365e18098fabdeb4d7f7
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-11-23 17:06:58 +0100

    rinkeby deploy

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,16 +1,5 @@
-const Ticket = artifacts.require("Ticket");
-
-module.exports = (deployer, network, accounts) => {
-  const [dealer, organizer] = accounts;
-  deployer.deploy(
-    Ticket,
-    dealer,
-    organizer,
-    100,
-    false,
-    10,
-    100,
-    "test",
-    "tst"
-  );
+const Market = artifacts.require("Market");
+const myAccount = process.env.ADMIN_WALLET_ADDRESS;
+module.exports = (deployer) => {
+  deployer.deploy(Market, { from: myAccount });
 };
